3dabd504fc805e3c055e3534a6a9098473f4ae86
Thierry Castro <thierrycastro555@gmail.com>
2022-11-01 13:43:12 -0300

    cadastro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const route = require("./routes");
-const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.use(route);
 
