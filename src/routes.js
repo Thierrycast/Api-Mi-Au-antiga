3bad3b40fa5c7b79557f4999b2ac6b3f387c4e49
Thierry Castro <thierrycastro555@gmail.com>
2022-11-11 13:25:05 -0300

    configuração de deploy 03

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,6 @@ const login = require("./controllers/login");
 const authorization = require("./middleware/authorization");
 const { registerUser, detailUser, updateUser } = require("./controllers/users");
 
-route.get("/", (req, res) => {
-  console.log("------------------ENTRAMOS CARAI----------------- ");
-  return res.status(200).send("VOCE CONSEGUIU!!");
-});
-
 route.post("/users", registerUser);
 
 route.post("/login", login);
