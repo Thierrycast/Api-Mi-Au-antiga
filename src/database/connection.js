4dc93b156e78d13a545f4edd4b2ac072b48efd37
Thierry Castro <thierrycastro555@gmail.com>
2022-11-11 13:59:51 -0300

    configuração de deploy 05

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,7 +6,7 @@ const knex = require("knex")({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     ssl: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: true,
     },
   },
 });
