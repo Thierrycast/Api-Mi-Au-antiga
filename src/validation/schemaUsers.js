85a93d4286b799caaf898046a0123c4cc7eac194
Thierry Castro <thierrycastro555@gmail.com>
2022-11-13 11:05:24 -0300

    correção erro validate

--- src/validation/schemaUsers.js
+++ src/validation/schemaUsers.js
@@ -4,7 +4,7 @@ const schemaRegisterUser = yup.object().shape({
   nome: yup.string().required(),
   email: yup.string().required().email(),
   senha: yup.string().required().min(6).max(10).trim(),
-  aniversario: yup.date().required(),
+  data: yup.date().required(),
 });
 
 const schemaUpdateUser = yup.object().shape({
