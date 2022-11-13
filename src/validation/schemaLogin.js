0df6e7668fd0f7dedeeff2cba73836bcb3950be1
Thierry Castro <thierrycastro555@gmail.com>
2022-11-13 11:11:50 -0300

    limpando projeto

--- src/validation/schemaLogin.js
+++
@@ -1,8 +0,0 @@
-const yup = require("./yupConfig");
-
-const schemaLogin = yup.object().shape({
-  email: yup.string().required().email(),
-  senha: yup.string().required().min(6).max(10).trim(),
-});
-
-module.exports = schemaLogin;
